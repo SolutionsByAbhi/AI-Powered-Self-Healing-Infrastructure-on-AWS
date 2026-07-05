336899680e7f96bf804d82bd37350e4e59f49b54
CloudNexus <abhighnagiri@gmail.com>
2026-07-05 20:05:44 +0530

    Create exceptions.py

---
+++ lambda/exceptions.py
@@ -0,0 +1,14 @@
+class PredictionException(Exception):
+    pass
+
+
+class RemediationException(Exception):
+    pass
+
+
+class ApprovalException(Exception):
+    pass
+
+
+class WorkflowException(Exception):
+    pass
