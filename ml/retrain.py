b511935b6fc98333fd0e98d6a08e8c33fd7cbe48
CloudNexus <abhighnagiri@gmail.com>
2026-07-05 20:19:42 +0530

    Create retrain.py

---
+++ ml/retrain.py
@@ -0,0 +1,24 @@
+import os
+
+import time
+
+
+while True:
+
+    print(
+
+        "Retraining..."
+
+    )
+
+    os.system(
+
+        "python train.py"
+
+    )
+
+    time.sleep(
+
+        86400
+
+    )
